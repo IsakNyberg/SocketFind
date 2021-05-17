18609c765a29416344db9f109e09035dbd2d679b
Isak Nyberg <36712644+IsakNyberg@users.noreply.github.com>
2021-05-18 00:57:56 +0200

    removed agario

--- field.py
+++ field.py
@@ -74,8 +74,6 @@ class Entity:
             if self.weakness == other.name:
                 self.damage += 1
                 other.points += 1
-                other.size -= 1
-                self.size += 1
                 return True
         return False
 
